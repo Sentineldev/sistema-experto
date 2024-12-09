99e01c3da63b88b5bc9da5157a3bc67d2bb6a22a
Joel Guillen <noctis2001@gmail.com>
2024-12-09 01:31:03 -0400

    Se agregó la funcionalidad de las preguntas y se renderizan correctamente (con el filtrado).

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,64 @@
+import Questions from "./Components/Questions"
+import Avatar from "./Components/Avatar"
+import { useState } from "react"
+import { QUESTIONS, QUESTION_KEY, ANSWERS_TYPE, SUCCESS_COMBINATIONS, QUESTIONS_FLOW } from "./Consts/const"
+
+
 function App() {
+  const [indexQuestion, setIndexQuestion] = useState<QUESTION_KEY>(1)
+  const [question, setQuestion] = useState(QUESTIONS[indexQuestion])
+  const [answerState, setAnswerState] = useState <ANSWERS_TYPE>(null)
+  let answers_values: Array<number | null> = Array(12).fill(null);
+
+  
+  function questionsDone(indexQuestion:QUESTION_KEY, answerState:ANSWERS_TYPE){
+    const newAnswerQuestion = [indexQuestion, answerState]
+    const isMatch = SUCCESS_COMBINATIONS.some(combination => 
+      combination[0] === newAnswerQuestion[0] && combination[1] === newAnswerQuestion[1]
+    );
+    return isMatch
+    
+  }
   
 
+  function nextQuestion(answerState: ANSWERS_TYPE, indexQuestion: QUESTION_KEY, answers_values: (number | null)[]){
+    
+    if(questionsDone(indexQuestion, answerState)){
+      return 
+    }else{
+   
+      const key = indexQuestion
+      if(key in QUESTIONS_FLOW){
+        const values = QUESTIONS_FLOW[key];
+       if(answerState !== null && answerState in values){
+         const newKeyQuestion = values[answerState] as QUESTION_KEY
+         setQuestion(QUESTIONS[newKeyQuestion]);  
+         setIndexQuestion(newKeyQuestion);
+       }
+      } 
+     
+    }
+    
+  }
+
+
+  function addAnswerQuestion(flag: ANSWERS_TYPE,indexQuestion: QUESTION_KEY, answers_values: (number | null)[]){
+    setAnswerState(flag);
+    nextQuestion(flag, indexQuestion, answers_values)
+  }
+
+ 
   return (
-    <>
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
+    <div className="flex flex-col items-center justify-center gap-4">
+        <Avatar/>
+        <Questions message={question}/> 
+      <div className="flex gap-10">
+        <button className="btn bg-red-700 w-14 hover:bg-red-900 h-12" onClick={() => addAnswerQuestion(0, indexQuestion, answers_values)} >No</button>
+        <button className="btn bg-success w-14 hover:bg-green-800 h-12" onClick={() => addAnswerQuestion(1, indexQuestion, answers_values)}>Sí</button>
       </div>
-    </>
+
+    
+    </div>
   )
 }
 
---
+++ client/src/Components/Questions.tsx
@@ -0,0 +1,14 @@
+type PropsQuestion = {
+    message: string
+}
+
+
+function Questions({message}:PropsQuestion){
+    return(
+        <p className="bg-sky-800 text-white min-h-16 p-4 h-auto rounded-lg text-sm w-96 ">
+            {message}
+        </p>
+    )
+}
+
+export default Questions;
