84885bc85e230239c6998f5f6b94e1ec60c414ab
Joel Guillen <noctis2001@gmail.com>
2024-12-09 06:15:50 -0400

    Refactorización, constantes para las combinaciones, para flujo de las combinaciones,, etc. Se renderizan las preguntas correctamente en la interfaz y se almacena los resultados en un estado.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,29 @@
-import Questions from "./Components/Questions"
-import Avatar from "./Components/Avatar"
-import { useState } from "react"
-import { QUESTIONS, QUESTION_KEY, ANSWERS_TYPE, SUCCESS_COMBINATIONS, QUESTIONS_FLOW } from "./Consts/const"
-
-
-function App() {
-  const [indexQuestion, setIndexQuestion] = useState<QUESTION_KEY>(1)
-  const [question, setQuestion] = useState(QUESTIONS[indexQuestion])
-  const [answerState, setAnswerState] = useState <ANSWERS_TYPE>(null)
-  let answers_values: Array<number | null> = Array(12).fill(null);
+  import Questions from "./Components/Questions";
+  import Avatar from "./Components/Avatar";
+  import useQuestion from "./hooks/useQuestion";
 
   
-  function questionsDone(indexQuestion:QUESTION_KEY, answerState:ANSWERS_TYPE){
-    const newAnswerQuestion = [indexQuestion, answerState]
-    const isMatch = SUCCESS_COMBINATIONS.some(combination => 
-      combination[0] === newAnswerQuestion[0] && combination[1] === newAnswerQuestion[1]
-    );
-    return isMatch
-    
-  }
-  
-
-  function nextQuestion(answerState: ANSWERS_TYPE, indexQuestion: QUESTION_KEY, answers_values: (number | null)[]){
-    
-    if(questionsDone(indexQuestion, answerState)){
-      return 
-    }else{
-   
-      const key = indexQuestion
-      if(key in QUESTIONS_FLOW){
-        const values = QUESTIONS_FLOW[key];
-       if(answerState !== null && answerState in values){
-         const newKeyQuestion = values[answerState] as QUESTION_KEY
-         setQuestion(QUESTIONS[newKeyQuestion]);  
-         setIndexQuestion(newKeyQuestion);
-       }
-      } 
-     
-    }
-    
-  }
-
-
-  function addAnswerQuestion(flag: ANSWERS_TYPE,indexQuestion: QUESTION_KEY, answers_values: (number | null)[]){
-    setAnswerState(flag);
-    nextQuestion(flag, indexQuestion, answers_values)
-  }
-
- 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-        <Avatar/>
-        <Questions message={question}/> 
-      <div className="flex gap-10">
-        <button className="btn bg-red-700 w-14 hover:bg-red-900 h-12" onClick={() => addAnswerQuestion(0, indexQuestion, answers_values)} >No</button>
-        <button className="btn bg-success w-14 hover:bg-green-800 h-12" onClick={() => addAnswerQuestion(1, indexQuestion, answers_values)}>Sí</button>
+  function App() {
+      const {indexQuestion, question, answersValues, addAnswerQuestion} = useQuestion()
+      console.log(answersValues)
+    return (
+      <div className="flex justify-center items-center w-full h-screen bg-white ">
+        <div className="flex flex-col items-center justify-center bg-red-700 rounded-xl h-[50vh] w-1/2 gap-4 pb-2">
+          <Avatar />
+          <Questions message={question} />
+          <div className="flex gap-10">
+            <button className="btn bg-red-700 w-16 hover:bg-red-900 h-16"
+              onClick={() => addAnswerQuestion(0, indexQuestion, answersValues)}> 
+              No
+            </button>
+            <button className="btn bg-success w-16 hover:bg-green-800 h-16" 
+              onClick={() => addAnswerQuestion(1, indexQuestion, answersValues)}>
+              Sí
+            </button>
+          </div>
+        </div>
       </div>
+    );
+  }
 
-    
-    </div>
-  )
-}
-
-export default App
+  export default App;
--- client/src/Components/Questions.tsx
+++ client/src/Components/Questions.tsx
@@ -5,7 +5,7 @@ type PropsQuestion = {
 
 function Questions({message}:PropsQuestion){
     return(
-        <p className="bg-sky-800 text-white min-h-16 p-4 h-auto rounded-lg text-sm w-96 ">
+        <p className="bg-sky-800 text-white min-h-16 p-4 h-auto rounded-lg text-base w-96 ">
             {message}
         </p>
     )
