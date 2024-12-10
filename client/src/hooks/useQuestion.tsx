abdf7a40501694c2048df87a6eb38b5b9bb847e2
Joel Guillen <noctis2001@gmail.com>
2024-12-10 12:27:03 -0400

    CREACIÓN DE VARIABLES DE ENTORNO Y REFACTORIZACIÓN DE LAS FUNCIONES DEL CUSTOMHOOK useQuestion

--- client/src/hooks/useQuestion.tsx
+++ client/src/hooks/useQuestion.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { QUESTIONS, QUESTION_KEY, ANSWERS_TYPE, SUCCESS_COMBINATIONS, QUESTIONS_FLOW } from "../Consts/const";
+import { QUESTIONS, QUESTION_KEY, ANSWERS_TYPE, SUCCESS_COMBINATIONS, QUESTIONS_FLOW, ANSWERS_VALUES } from "../Consts/const";
 
 
 function useQuestion(){
@@ -7,6 +7,15 @@ function useQuestion(){
     const [question, setQuestion] = useState(QUESTIONS[indexQuestion]);
     const [answerState, setAnswerState] = useState<ANSWERS_TYPE>(null);
     const [answersValues, setAnswersValues] = useState<Array<number | null>>(Array(12).fill(null)); 
+
+    function addAnswerQuestion(answerState: ANSWERS_TYPE,indexQuestion: QUESTION_KEY, answersValues: Array<number | null>) {
+        setAnswerState(answerState);
+        nextQuestion(answerState, indexQuestion, answersValues);
+    }
+    
+    function addAnswerToArray(updateAnswersValues:ANSWERS_VALUES, answerState:ANSWERS_TYPE, indexQuestion: QUESTION_KEY){
+        answerState === 0 ? updateAnswersValues[indexQuestion-1] = 0 : updateAnswersValues[indexQuestion-1] = 1;
+    }
 
     function questionsDone(indexQuestion: QUESTION_KEY, answerState: ANSWERS_TYPE) {
         const newAnswerQuestion = [indexQuestion, answerState];
@@ -16,28 +25,33 @@ function useQuestion(){
         return isMatch;
     }
 
-    function nextQuestion(answerState: ANSWERS_TYPE, indexQuestion: QUESTION_KEY, answersValues: Array<number | null>) {
-        const updatedAnswersValues = [...answersValues];
-
-        if (answerState === 0) {
-        updatedAnswersValues[indexQuestion - 1] = 0;
-        } 
-        else {
-        updatedAnswersValues[indexQuestion - 1] = 1;
-        }
-        
-        if (questionsDone(indexQuestion, answerState)) {
+    function fillArrayWithZeros(updatedAnswersValues: ANSWERS_VALUES  ){
         const finalAnswersValues = updatedAnswersValues.map((value) => 
             value === null ? 0 : value
         );
-            setAnswersValues(finalAnswersValues);
-            return;
-        } 
-        else {
-        
-            setAnswersValues(updatedAnswersValues);  
-            const key = indexQuestion;
-            if (key in QUESTIONS_FLOW) {
+        return finalAnswersValues
+    }
+
+    function postFinalAnswersValues(finalAnswersValues:number[] ){
+        const url = `${import.meta.env.URL_API}/${import.meta.env.KEY_STRING_RESULT}`
+        const data = {"answers": finalAnswersValues}      
+        fetch(url, {
+            method: "POST", 
+            body: JSON.stringify(data), 
+            headers: {
+            "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .catch((error) => console.error("Error:", error))
+            .then((response) => console.log("Success:", response));
+        setAnswersValues(finalAnswersValues);
+    }
+
+    function serchQuestionInArray(indexQuestion: QUESTION_KEY, answerState: ANSWERS_TYPE, updatedAnswersValues: ANSWERS_VALUES){
+        setAnswersValues(updatedAnswersValues);  
+        const key = indexQuestion;
+        if (key in QUESTIONS_FLOW) {
             const values = QUESTIONS_FLOW[key];
             if (answerState !== null && answerState in values) {
                 const newKeyQuestion = values[answerState] as QUESTION_KEY;
@@ -45,16 +59,22 @@ function useQuestion(){
                 setIndexQuestion(newKeyQuestion);
             }
         }
+    }
+
+    function nextQuestion(answerState: ANSWERS_TYPE, indexQuestion: QUESTION_KEY, answersValues: ANSWERS_VALUES) {
+        const updatedAnswersValues = [...answersValues];
+        addAnswerToArray(updatedAnswersValues, answerState, indexQuestion)
+        
+        if (questionsDone(indexQuestion, answerState)) {
+            const finalAnswersValues = fillArrayWithZeros(updatedAnswersValues)
+            postFinalAnswersValues(finalAnswersValues)
+            return;
+        } 
+        else {
+           serchQuestionInArray(indexQuestion, answerState, updatedAnswersValues)
         }
     }
-    
-
-    function addAnswerQuestion(flag: ANSWERS_TYPE,indexQuestion: QUESTION_KEY, answersValues: Array<number | null>) {
-        setAnswerState(flag);
-        nextQuestion(flag, indexQuestion, answersValues);
-    }
-
-
+       
     return {indexQuestion, question, answerState, answersValues, addAnswerQuestion}
 }
 
