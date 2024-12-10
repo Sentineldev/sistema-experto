a25e4f0c3cf8da341163f1bba2a72dafc4f3ede3
Sentineldev <figueraj196@gmail.com>
2024-12-10 13:04:29 -0400

    cors

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,12 @@
 from flask import Flask
- 
+from flask_cors import CORS
 
 from api import api
 from internal import internal
 def create_app():
     
     app = Flask(__name__)
-    
+    CORS(app)
     
     app.register_blueprint(internal)
     app.register_blueprint(api)
