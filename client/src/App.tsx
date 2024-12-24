d28468d5bce31cfce82aa598c76da6d35e95090d
Joel Guillen <noctis2001@gmail.com>
2024-12-24 03:56:25 -0400

    Se agregó la funcionalidad para que el avatar hable.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
 import Questions from "./Components/Questions";
 import Avatar from "./Components/Avatar";
 import useQuestion from "./hooks/useQuestion";
 import { BaseButton} from "./Components/Buttons/BaseButton";
 import { PreviousButton } from "./Components/Buttons/PreviousButton";
+import { ButtonSpeaker } from "./Components/Buttons/ButtonSpeaker";
 import { FaRegCircleCheck } from "react-icons/fa6";
 import { GiCancel } from "react-icons/gi";
 import { GrPrevious } from "react-icons/gr";
+import { PiSpeakerSimpleHighFill } from "react-icons/pi";
+
 
 function App() {
     const { indexQuestion, question, answersValues, addAnswerQuestion, goBack } = useQuestion();
+    const [isSpeaking, setIsSpeaking] = useState(false)
+    
+    function handleSpeak(question: string){
+        const utterance = new SpeechSynthesisUtterance(question);
+        window.speechSynthesis.cancel();
+        utterance.lang = "es-ES";
+        utterance.rate = 1; 
+        utterance.pitch = 1; 
+        utterance.onstart = () => setIsSpeaking(true);
+        utterance.onend = () => setIsSpeaking(false);
+        window.speechSynthesis.speak(utterance);
+    }
+
     return (
         <div className="h-screen w-screen flex justify-center">
             <div className="h-screen w-1/2 flex flex-col items-center justify-center">
                 <div className="w-full text-center p-6 bg-gradient-to-r from-blue-500 to-purple-600 rounded-t-lg">
-                    <Avatar />
+                    <Avatar isSpeaking={isSpeaking} />
                     <h2 className="mt-4 text-xl font-bold text-white  ml-4">  Pregunta: {indexQuestion}</h2>
                 </div>
-                <div className="w-full h-1/2 bg-white flex flex-col rounded-b-lg">
-                   <div className="flex items-center justify-between mb-6">
-                        <PreviousButton text="Anterior" icon={<GrPrevious className="mr-2"/>} visibility={indexQuestion>1? "visible": "invisible"} onClick={goBack}/>
+                <div className="w-full h-1/2 bg-white flex flex-col rounded-b-lg ">
+                   <div className="flex items-center justify-around mb-6">
+                        {indexQuestion > 1 && <PreviousButton text="Anterior" icon={<GrPrevious className="mr-2"/>}  onClick={goBack}/>}
+                        <ButtonSpeaker onClick={() => handleSpeak(question)} icon={<PiSpeakerSimpleHighFill className="size-7 text-gray-600"/>}/>
                     </div>
                   
                     <div className="w-full flex justify-center h-1/5">
@@ -30,6 +48,7 @@ function App() {
                         <BaseButton onClick={() => addAnswerQuestion(1, indexQuestion, answersValues)} 
                             text={indexQuestion === 4 ? "Adhesivas" : "Sí"} icon={<FaRegCircleCheck className="w-4 h-4 text-gray-400"/>}/> 
                     </div>
+                    
                 </div>
             </div>
         </div>
--- client/src/Components/Buttons/PreviousButton.tsx
+++ client/src/Components/Buttons/PreviousButton.tsx
@@ -4,12 +4,11 @@ interface PropsPreviosButton{
   text: string,
   onClick: () => void, 
   icon: ReactNode,
-  visibility: string
 }
 
-export function PreviousButton({text, onClick, icon, visibility}:PropsPreviosButton){
+export function PreviousButton({text, onClick, icon}:PropsPreviosButton){
   return(
-    <button  className={`w-auto p-4 text-left rounded-lg transition-colors duration-200 bg-gray-50 hover:bg-gray-100 text-gray-700 ${visibility}`} onClick={onClick}>
+    <button  className={`w-auto p-4 text-left rounded-lg transition-colors duration-200 bg-gray-50 hover:bg-gray-100 text-gray-700`} onClick={onClick}>
         <span className="flex items-center">
           {icon} {text}
         </span>
