a42c7c4d49dfe2777b74e8b228c63120b04275b5
Joel Guillen <noctis2001@gmail.com>
2025-01-31 00:58:33 -0400

    Se agregaron keyframes y animaciones para mover el componente Pulser hacia la derecha.

--- client/src/Components/Pulser/Pulser.tsx
+++ client/src/Components/Pulser/Pulser.tsx
@@ -2,7 +2,7 @@ export function Pulser() {
   return (
     <div>
       <div className="flex gap-3 justify-center">
-        <div className="bg-blue-300 p-3 rounded-md shadow-yellow-400 shadow-2xl relative">
+        <div className="bg-blue-300 p-3 rounded-md shadow-yellow-400 shadow-2xl relative animate-move-right">
           <h2 className="text-center text-black">Se ha encontrado un Filum.</h2>
           <div className="bg-yellow-300 rounded-full w-5 h-5 absolute left-[194px] bottom-8">
             <div className="bg-yellow-300  rounded-full animate-ping-effect w-full h-full">
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,11 +12,16 @@ export default {
         'ping-scale': {
           '0%': { opacity: 0 },
           '50%': { opacity: .4, transform: 'scale(1.7)' },
+        },
+        'move-right': {
+          'from': { transform: 'translateX(-100px)' },
+          'to': { transform: 'translateX(0px)' }
         }
 
       },
       animation: {
-        'ping-effect': 'ping-scale 2s ease-in-out infinite'
+        'ping-effect': 'ping-scale 2s ease-in-out infinite',
+        'move-right': 'move-right .4s ease-in 1'
 
       }
     },
