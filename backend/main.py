e2e31594436b0c1030e52df1dd3bdafaa24fb0fa
Sentineldev <figueraj196@gmail.com>
2025-02-03 20:49:44 -0400

    added .env

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,9 @@
 from app import app
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+
 if __name__ == "__main__":
-    app.run(host="localhost",port=5000,debug=False)
+
+    port = getenv("PORT")
+    app.run(host="localhost",port=port,debug=False)
